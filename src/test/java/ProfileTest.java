9e7ed02647e74dd179182cb0e05fee74d15353b6
Kedlawv <waldek.bus@gmail.com>
2019-06-17 15:35:10 +0200

    Profile class tests, few more test practice Just before refactor for @After

--- src/test/java/ProfileTest.java
+++ src/test/java/ProfileTest.java
@@ -1,23 +1,79 @@
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
 
 public class ProfileTest {
 
+    private enum TestBoolean {
+        TRUE(1),FALSE(0);
+        private int value;
+
+        TestBoolean(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private Profile profile;
+    private Criteria criteria;
+
+    private Question questionReimbursesTuition;
+    private Answer answerReimbursesTuition;
+    private Answer answerDoesNotReimburseTuition;
+
+    private Question questionIsThereRelocation;
+    private Answer answerThereIsRelocation;
+    private Answer answerThereIsNoRelocation;
+
+    private Question questionOnsiteDaycare;
+    private Answer answerNoOnsiteDaycare;
+    private Answer answerHasOnsiteDaycare;
+
+    @Before
+    public void createProfile (){profile = new Profile("Bull Hockey, Inc.");}
+
+    @Before
+    public void createCriteria(){criteria = new Criteria();}
+
+
+
+    @Before
+    public void createQuestionsAndAnswers(){
+
+        questionIsThereRelocation =
+                new BooleanQuestion(1, "Relocation package?");
+        answerThereIsRelocation =
+                new Answer(questionIsThereRelocation, TestBoolean.TRUE.getValue());
+        answerThereIsNoRelocation =
+                new Answer(questionIsThereRelocation, TestBoolean.FALSE.getValue());
+
+        questionReimbursesTuition =
+                new BooleanQuestion(1, "Reimburses tuition?");
+        answerReimbursesTuition =
+                new Answer(questionReimbursesTuition, TestBoolean.TRUE.getValue());
+        answerDoesNotReimburseTuition =
+                new Answer(questionReimbursesTuition, TestBoolean.FALSE.getValue());
+
+        questionOnsiteDaycare =
+                new BooleanQuestion(1, "Onsite daycare?");
+        answerHasOnsiteDaycare =
+                new Answer(questionOnsiteDaycare, TestBoolean.TRUE.getValue());
+        answerNoOnsiteDaycare =
+                new Answer(questionOnsiteDaycare, TestBoolean.FALSE.getValue());
+    }
+
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "Got bonuses?");
-        Answer profileAnswer = new Answer(question, 0);
-        profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
-        Answer criteriaAnswer = new Answer(question, 1);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-        criteria.add(criterion);
+
+        profile.add(new Answer(questionIsThereRelocation, 0));
+        criteria.add(new Criterion(new Answer(questionIsThereRelocation, 1), Weight.MustMatch));
 
         boolean match = profile.matches(criteria);
-
 
         assertFalse(match);
     }
@@ -25,14 +81,8 @@ public class ProfileTest {
     @Test
     public void matchAnswersTrueForAnyDontCareCriteria() {
         //Arrange
-        Profile profile = new Profile("Bull Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "Got milk?");
-        Answer profileAnswer = new Answer(question, 0);
-        profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
-        Answer criteriaAnswer = new Answer(question, 1);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-        criteria.add(criterion);
+        profile.add(new Answer(questionIsThereRelocation, 0));
+        criteria.add(new Criterion(new Answer(questionIsThereRelocation, 1), Weight.DontCare));
 
         //Act
         boolean matches = profile.matches(criteria);
@@ -40,5 +90,33 @@ public class ProfileTest {
         //Assert
         assertTrue(matches);
 
+    }
+
+
+    @Test
+    public void scoreAccumulatesCriterionValuesForMatches() {
+        profile.add(answerThereIsRelocation);
+        profile.add(answerReimbursesTuition);
+        profile.add(answerNoOnsiteDaycare);
+        criteria.add(new Criterion(answerThereIsRelocation, Weight.WouldPrefer));
+        criteria.add(new Criterion(answerReimbursesTuition, Weight.Important));
+        criteria.add(new Criterion(answerHasOnsiteDaycare, Weight.VeryImportant));
+
+        profile.matches(criteria);
+
+        int expectedScore = Weight.WouldPrefer.getValue() + Weight.Important.getValue();
+        assertThat(profile.score(), equalTo(expectedScore));
+    }
+
+    @Test
+    public void matchAnswersFalseWhenNoneOfMultipleCriteriaMatch() {
+        profile.add(answerDoesNotReimburseTuition);
+        profile.add(answerNoOnsiteDaycare);
+        criteria.add(new Criterion(answerReimbursesTuition,Weight.VeryImportant));
+        criteria.add(new Criterion(answerHasOnsiteDaycare, Weight.Important));
+
+        boolean result = profile.matches(criteria);
+
+        assertFalse(result);
     }
 }
