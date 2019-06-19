2940c21ae8a9c31d094f991f0f6291436e2e67c6
Kedlawv <waldek.bus@gmail.com>
2019-06-19 17:27:39 +0200

    matchesWhenProfileContainsMatchingAnswer() plus creation of profile refactored to @Before createProfile()

--- src/main/java/tdd/Profile.java
+++ src/main/java/tdd/Profile.java
@@ -1,7 +1,19 @@
 package tdd;
 
 public class Profile {
+    private Answer answer;
+
     public boolean matches(Criterion criterion) {
+        if(answer != null){
+            return true;
+
+        }
+
         return false;
+
+    }
+
+    public void add(Answer answer) {
+        this.answer = answer;
     }
 }
--- src/test/java/tdd/ProfileTest.java
+++ src/test/java/tdd/ProfileTest.java
@@ -1,18 +1,39 @@
 package tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ProfileTest {
+
+    private Profile profile;
+
+    @Before
+    public void createProfile(){
+        profile = new Profile();
+    }
+
     @Test
     public void mathesNothingWhenProfileEmpty(){
-        Profile profile = new Profile();
         Question question = new BooleanQuestion(1, "Relocation Package");
         Criterion criterion = new Criterion(new Answer(question, 1),Weight.DontCare);
 
         boolean result = profile.matches(criterion);
 
         assertFalse(result);
+    }
+
+    @Test
+    public void matchesWhenProfileContainsMatchingAnswer(){
+        Question question = new BooleanQuestion(1, "Relocation Package");
+        Answer answer = new Answer(question,1);
+        Criterion criterion = new Criterion(answer,Weight.Important);
+        profile.add(answer);
+
+        boolean result = profile.matches(criterion);
+
+        assertTrue(result);
     }
 }
